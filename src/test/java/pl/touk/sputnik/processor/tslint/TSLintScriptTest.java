8dd0c502b4c3ba0196666e6643ef4e2def849da7
GabTouchette[core] <gtouchette@powergo.ca>
2024-02-12 16:42:28 -0500

    remove test

--- src/test/java/pl/touk/sputnik/processor/tslint/TSLintScriptTest.java
+++ src/test/java/pl/touk/sputnik/processor/tslint/TSLintScriptTest.java
@@ -2,10 +2,8 @@ package pl.touk.sputnik.processor.tslint;
 
 import com.google.common.collect.ImmutableMap;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import pl.touk.sputnik.configuration.Configuration;
 import pl.touk.sputnik.configuration.ConfigurationSetup;
 import pl.touk.sputnik.configuration.GeneralOption;
 
@@ -37,22 +35,5 @@ class TSLintScriptTest {
         Throwable thrown = catchThrowable(tsLint::validateConfiguration);
 
         assertThat(thrown).isNull();
-    }
-
-    @Test
-    void shouldReturnViolationsInJSONFormat() {
-        final String configFile = "src/main/resources/tslint.json";
-        new ConfigurationSetup().setUp(ImmutableMap.of(GeneralOption.TSLINT_CONFIGURATION_FILE.getKey(), configFile));
-        Configuration config = new ConfigurationSetup().setUp(ImmutableMap.of(
-                GeneralOption.TSLINT_SCRIPT.getKey(), "tslint",
-                GeneralOption.TSLINT_CONFIGURATION_FILE.getKey(), "src/main/resources/tslint.json"));
-
-        String tsScript = config.getProperty(GeneralOption.TSLINT_SCRIPT);
-        TSLintScript tsLint = new TSLintScript(tsScript, configFile);
-
-        String filePath = "src/main/resources/tslint/testFile.ts";
-        String violationsJson = tsLint.reviewFile(filePath);
-
-        assertThat(violationsJson).isNotNull();
     }
 }
